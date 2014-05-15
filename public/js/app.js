a3c5aaa50677123e4704117407f92c674e0268f1
thetmkay <george@upstatement.com>
2014-05-15 17:49:08 -0400

    first implementation of stream idea

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,40 @@
 
 	'use strict';
 
+	$('.blox').on('click', function(event) {
+		// $(this).css({
+		// 	display: "block"
+		// });
+		$(this).removeClass('not-full');
+		$(this).addClass('full');
+		$(this).unbind('click');
+
+		var previous = $(this).prevUntil('.full'),
+			prev_width = (100/previous.length);
+
+		previous.each(function(index,element){
+			if(previous.length === 1) {
+				$(element).removeClass('not-full')
+				$(element).addClass('full');
+			}
+
+			$(element).css({
+				width: prev_width + '%'
+			});
+		});
+
+		var next = $(this).nextUntil('.full'),
+			next_width = 100/next.length;
+		next.each(function(index,element){
+			if(next.length === 1) {
+				$(element).removeClass('not-full')
+				$(element).addClass('full');
+			}
+
+			$(element).css({
+				width: next_width + '%'
+			});
+		});
+	});
+
 })(jQuery);

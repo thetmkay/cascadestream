786059f0954eda1fa416b1b55356777be52c9645
thetmkay <george@upstatement.com>
2014-05-21 16:55:52 -0400

    Working prototype for cascade stream

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,10 @@
 	'use strict';
 
 	$('.blox').on('click', function(event) {
+		var $this = $(this);
+		if($this.hasClass('full')) {
+			return
+		}
 		// $(this).css({
 		// 	display: "block"
 		// });
@@ -10,32 +14,41 @@
 		$(this).addClass('full');
 		$(this).unbind('click');
 
-		var previous = $(this).prevUntil('.full'),
-			prev_width = (100/previous.length);
+		// $this.detach();
 
-		previous.each(function(index,element){
-			if(previous.length === 1) {
+		var $prev = $this.prevAll('.full');
+
+		if($prev.length > 0)
+			$prev.first().after($this);
+		else
+			$this.parent().prepend($this);
+
+		var $siblings = $this.siblings('.not-full'),
+			sibl_width = 100/$siblings.length;
+
+		$siblings.each(function(index,element){
+			if($siblings.length === 1) {
 				$(element).removeClass('not-full')
 				$(element).addClass('full');
 			}
 
 			$(element).css({
-				width: prev_width + '%'
+				width: (sibl_width-1) + '%'
 			});
 		});
 
-		var next = $(this).nextUntil('.full'),
-			next_width = 100/next.length;
-		next.each(function(index,element){
-			if(next.length === 1) {
-				$(element).removeClass('not-full')
-				$(element).addClass('full');
-			}
+		// var next = $(this).nextUntil('.full'),
+		// 	next_width = 100/next.length;
+		// next.each(function(index,element){
+		// 	if(next.length === 1) {
+		// 		$(element).removeClass('not-full')
+		// 		$(element).addClass('full');
+		// 	}
 
-			$(element).css({
-				width: next_width + '%'
-			});
-		});
+		// 	$(element).css({
+		// 		width: next_width + '%'
+		// 	});
+		// });
 	});
 
 })(jQuery);

8350fb733886555e1cd160050540dded12a2fa39
thetmkay <george@upstatement.com>
2014-05-22 19:28:10 -0400

    class prefixed to avoid css selector clashes

--- public/js/app.js
+++ public/js/app.js
@@ -2,34 +2,34 @@
 
 	'use strict';
 
-	$('.blox').on('click', function(event) {
+	$('.cs-block').on('click', function(event) {
 		var $this = $(this);
-		if($this.hasClass('full')) {
+		if($this.hasClass('cs-full')) {
 			return
 		}
 		// $(this).css({
 		// 	display: "block"
 		// });
-		$(this).removeClass('not-full');
-		$(this).addClass('full');
+		$(this).removeClass('cs-not-full');
+		$(this).addClass('cs-full');
 		$(this).unbind('click');
 
 		// $this.detach();
 
-		var $prev = $this.prevAll('.full');
+		var $prev = $this.prevAll('.cs-full');
 
 		if($prev.length > 0)
 			$prev.first().after($this);
 		else
 			$this.parent().prepend($this);
 
-		var $siblings = $this.siblings('.not-full'),
+		var $siblings = $this.siblings('.cs-not-full'),
 			sibl_width = 100/$siblings.length;
 
 		$siblings.each(function(index,element){
 			if($siblings.length === 1) {
-				$(element).removeClass('not-full')
-				$(element).addClass('full');
+				$(element).removeClass('cs-not-full')
+				$(element).addClass('cs-full');
 			}
 
 			$(element).css({

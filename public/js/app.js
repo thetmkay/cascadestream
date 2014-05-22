33ff7ef3930672dbd432fc2738533846ebb0aa76
thetmkay <george@upstatement.com>
2014-05-22 19:40:24 -0400

    Refactored in to a jquery extended fn

--- public/js/app.js
+++ public/js/app.js
@@ -2,53 +2,6 @@
 
 	'use strict';
 
-	$('.cs-block').on('click', function(event) {
-		var $this = $(this);
-		if($this.hasClass('cs-full')) {
-			return
-		}
-		// $(this).css({
-		// 	display: "block"
-		// });
-		$(this).removeClass('cs-not-full');
-		$(this).addClass('cs-full');
-		$(this).unbind('click');
-
-		// $this.detach();
-
-		var $prev = $this.prevAll('.cs-full');
-
-		if($prev.length > 0)
-			$prev.first().after($this);
-		else
-			$this.parent().prepend($this);
-
-		var $siblings = $this.siblings('.cs-not-full'),
-			sibl_width = 100/$siblings.length;
-
-		$siblings.each(function(index,element){
-			if($siblings.length === 1) {
-				$(element).removeClass('cs-not-full')
-				$(element).addClass('cs-full');
-			}
-
-			$(element).css({
-				width: (sibl_width-1) + '%'
-			});
-		});
-
-		// var next = $(this).nextUntil('.full'),
-		// 	next_width = 100/next.length;
-		// next.each(function(index,element){
-		// 	if(next.length === 1) {
-		// 		$(element).removeClass('not-full')
-		// 		$(element).addClass('full');
-		// 	}
-
-		// 	$(element).css({
-		// 		width: next_width + '%'
-		// 	});
-		// });
-	});
+	$(document).cascadeStream();
 
 })(jQuery);
